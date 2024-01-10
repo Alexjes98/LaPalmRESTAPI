c1562fb755d5a489beffefe497291be4baa529cf
Alejandro Lopez <alexjes6543@gmail.com>
2024-01-10 14:45:17 -0400

    add employees endpoint

--- src/controllers/employee.js
+++ src/controllers/employee.js
@@ -1,5 +1,4 @@
 const { Employee } = require("../models");
-
 const createEmployee = async (req, res) => {
   try {
     const { name, code, payrollType, companyId, positionId } = req.body;
@@ -30,8 +29,10 @@ const createEmployee = async (req, res) => {
 };
 const getEmployeeById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const employee = await Employee.findOne({ where: { id } });
+    const { id, companyId } = req.params;
+    const employee = await Employee.findOne({
+      where: { id, companyId },
+    });
     res
       .status(200)
       .json({ data: employee, message: "Employee retrieved successfully" });
@@ -49,9 +50,23 @@ const getAllEmployees = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+const getEmployeesByCompany = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const employees = await Employee.findAll({
+      where: { companyId: id },
+    });
+    res
+      .status(200)
+      .json({ data: employees, message: "Employees retrieved successfully" });
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
 
 module.exports = {
   createEmployee,
   getAllEmployees,
   getEmployeeById,
+  getEmployeesByCompany,
 };
--- src/routes/employees.js
+++ src/routes/employees.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { employeeController } = require("../controllers");
 
 router.get("/", employeeController.getAllEmployees);
+router.get("/company/:id", employeeController.getEmployeesByCompany);
+router.get("/company/:id/employee/:id", employeeController.getEmployeeById);
 router.post("/", employeeController.createEmployee);
 
 module.exports = router;
