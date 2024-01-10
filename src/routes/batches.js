c316b6dfebab4135b6f7560f1a009bd9a133f86d
Alejandro Lopez <alexjes6543@gmail.com>
2024-01-10 14:56:31 -0400

    fix batches endpoint

--- src/routes/batches.js
+++ src/routes/batches.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { batchController } = require("../controllers");
 
 router.get("/", batchController.getAllBatches);
-router.get("company/:id", batchController.getBatchesByCompany);
-router.get("company/:id/batch/:id", batchController.getBatchById);
+router.get("/company/:id", batchController.getBatchesByCompany);
+router.get("/company/:id/batch/:id", batchController.getBatchById);
 router.post("/", batchController.createBatch);
 
 module.exports = router;
